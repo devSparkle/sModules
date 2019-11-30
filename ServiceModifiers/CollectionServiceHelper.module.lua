a8cba1479e313aed8e3021b3a87b7d3594b4a401
Ricardo Loureiro <ricardo@devsparkle.me>
2019-11-30 17:48:31 +0000

    CollectionServiceHelper will now return a RBXScriptConnection

--- ServiceModifiers/CollectionServiceHelper.module.lua
+++ ServiceModifiers/CollectionServiceHelper.module.lua
@@ -7,17 +7,19 @@ local Module = {}
 --// Functions
 
 function Module.BindToTag(Tag, Callback)
-	CollectionService:GetInstanceAddedSignal(Tag):Connect(Callback)
+	spawn(function()
+		for _, TaggedItem in next, CollectionService:GetTagged(Tag) do
+			spawn(function()
+				Callback(TaggedItem)
+			end)
+		end
+	end)
 	
-	for _, TaggedItem in next, CollectionService:GetTagged(Tag) do
-		spawn(function()
-			Callback(TaggedItem)
-		end)
-	end
+	return CollectionService:GetInstanceAddedSignal(Tag):Connect(Callback)
 end
 
 function Module.BindToTagRemoved(Tag, Callback)
-	CollectionService:GetInstanceRemovedSignal(Tag):Connect(Callback)
+	return CollectionService:GetInstanceRemovedSignal(Tag):Connect(Callback)
 end
 
 return Module
